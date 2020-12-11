2a23f3fd8f564d7a4e9a1eebb81a159c097d0266
bioseq-tau <josefspr@gmail.com>
2020-12-11 14:05:37 +0200

    updates by Josef: UI changes, model names, job name, google analytics

--- js/googleAnalytics.js
+++ js/googleAnalytics.js
@@ -3,6 +3,6 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-66958010-1', 'auto');
+  ga('create', 'UA-185370879-1', 'auto');
   ga('send', 'pageview');
 
